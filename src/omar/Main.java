ef4d377250bb68cd60f4075a55ca6543642b6d69
EngOmarSoudi <92035173+EngOmarSoudi@users.noreply.github.com>
2022-12-11 11:05:45 +0300

    hacker rank tasks

--- src/omar/Main.java
+++ src/omar/Main.java
@@ -1,37 +1,136 @@
 package omar;
 
 import java.awt.*;
+import java.sql.Array;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Scanner;
 
+import static java.util.Arrays.*;
+
 public class Main {
     public static void main(String[] args) {
+        LearnBasics a = new LearnBasics();
+//        a.basics();
+//        System.out.println("Mortgage: " + calc());
+//        a.plusMinus();
+        a.miniMaxSum();
 
-
-        int age = 25;
-        Date now = new Date();
-        Point point1 = new Point(1,2);
-        Point point2 = point1;
-        point1.x=3;
-        int[] myArray={1,2};
-        System.out.println(Arrays.toString(myArray) );
-        //input
-        Scanner scanner=new Scanner(System.in);
-        System.out.print("Enter your name : ");
-        String name = scanner.nextLine().trim();
-        System.out.print("Enter your age : ");
-        age = scanner.nextInt();
-        System.out.println("you are : "+name + "your age is " + age);
-
-
-        System.out.println("Hello ali your age is "+ age+" " + now+" " + point2);
-        String c ;
-        c= calc();
-        System.out.println("Mortgage: " + c );
 
     }
+    public static class LearnBasics{
+        public void basics(){
+            int age = 25;
+            Date now = new Date();
+            Point point1 = new Point(1,2);
+            Point point2 = point1;
+            point1.x=3;
+            int[] myArray={1,2};
+            System.out.println(Arrays.toString(myArray) );
+            //input
+            Scanner scanner=new Scanner(System.in);
+            System.out.print("Enter your name : ");
+            String name = scanner.nextLine().trim();
+            System.out.print("Enter your age : ");
+            age = scanner.nextInt();
+            System.out.println("you are : "+name + "your age is " + age);
+
+
+            System.out.println("Hello ali your age is "+ age+" " + now+" " + point2);
+        }
+        //hacker rank task
+        public void plusMinus(){
+            int number;
+            double totalng =0,totalze=0,totalpu=0,dpu=0,dne=0,dze=0;
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("enter the number of elements you want to add");
+            number=scanner.nextInt();
+            int[] arry=new int[number+1];
+            System.out.println("Enter the elements of the array: ");
+            for (int i=0;i<number;i++){
+                arry[i]=scanner.nextInt();
+            }
+
+//        System.out.print(arr.length);
+            for (int j : arry) {
+                int n = 0;
+
+                if (j < 0) {
+
+                    totalng++;
+                    dne = totalng / arry.length;
+
+                }
+                if (j == 0) {
+
+                    totalze++;
+                    dze = (totalze-1) / arry.length;
+
+                }
+                if (j > 0) {
+
+                    totalpu++;
+                    dpu = totalpu / arry.length;
+                }
+
+
+            }
+            System.out.println("plus "+dpu);
+            System.out.println("negative "+dne);
+            System.out.println("zero "+dze);
+//            System.out.println(totalze);
+//            System.out.println(totalng);
+//            System.out.println(totalpu);
+        }
+
+//        hacker rank task 2
+        public void miniMaxSum(){
+            int number;
+
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("enter the number of elements you want to add");
+            number=scanner.nextInt();
+            int[] c = new int[number];
+            int[] arry1=new int[number];
+            System.out.println("Enter the elements of the array: ");
+            for (int i=0;i<number;i++){
+                arry1[i]=scanner.nextInt();
+            }
+            System.out.println(Arrays.toString(arry1));
+            c=arry1;
+            Arrays.sort(c);
+            System.out.println(Arrays.toString(c));
+            long sum = 0;
+            int min = c[0];
+            int max = c[0];
+            for(int i=0;i<c.length;i++){
+                sum+=c[i];
+                if(c[i]<min){
+                    min = c[i];
+                }
+                if(c[i]>max){
+                    max = c[i];
+                }
+            }
+            System.out.println((sum-max)+" "+(sum-min));
+            System.out.println(sum);
+        }
+//            long min = 0, max = 0;
+//            int count=0;
+//            for(int i = 0, j = c.length - 1; i < c.length - 1; i++, j--){
+//                max = max + c[j];
+//                min = min + c[i];
+//            }
+//            System.out.println(min + " " + max);
+//            System.out.println(count);
+
+
+
+
+    }
+
     public static String calc(){
         final byte MONTHS_IN_YEAR=12;
         final byte PERCENT=100;
@@ -50,34 +149,6 @@ public class Main {
         String mortageFormatted= NumberFormat.getCurrencyInstance().format(mortage);
         return mortageFormatted;
     }
-    public void plusMinus(){
-        int[] arr={-4 ,3, -9, 0, 4, 1}; double totalng =0,totalze=0,totalpu=0,dpu=0,dne=0,dze=0;
-//        System.out.print(arr.length);
-        for (int i=0;i<arr.length;i++){
-            int n = 0;
 
-            if (arr[i]<0){
-
-                totalng++;
-                dne =totalng / arr.length;
-
-            }
-            if(arr[i]==0){
-
-                totalze++;
-                dze =totalze/ arr.length;
-
-            }
-            if(arr[i]>0){
-
-                totalpu++;
-                dpu =totalpu/ arr.length;
-            }
-
-
-        }
-        System.out.println("pus "+dpu);
-        System.out.println("neg "+dne);
-        System.out.println("zero "+dze);
-    }
+   //hacker rank task
 }
