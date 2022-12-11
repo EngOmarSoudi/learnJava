aa6a4fde0f9de93433e723566f09788d414c96c6
EngOmarSoudi <92035173+EngOmarSoudi@users.noreply.github.com>
2022-12-11 12:24:48 +0300

    hacker rank timeConvert

--- src/omar/Main.java
+++ src/omar/Main.java
@@ -13,10 +13,13 @@ import static java.util.Arrays.*;
 public class Main {
     public static void main(String[] args) {
         LearnBasics a = new LearnBasics();
+        HackerRankTasks task=new HackerRankTasks();
+
 //        a.basics();
 //        System.out.println("Mortgage: " + calc());
-//        a.plusMinus();
-        a.miniMaxSum();
+//        task.plusMinus();
+//        task.miniMaxSum();
+        task.timeConversion();
 
 
     }
@@ -40,94 +43,6 @@ public class Main {
 
             System.out.println("Hello ali your age is "+ age+" " + now+" " + point2);
         }
-        //hacker rank task
-        public void plusMinus(){
-            int number;
-            double totalng =0,totalze=0,totalpu=0,dpu=0,dne=0,dze=0;
-            Scanner scanner=new Scanner(System.in);
-            System.out.println("enter the number of elements you want to add");
-            number=scanner.nextInt();
-            int[] arry=new int[number+1];
-            System.out.println("Enter the elements of the array: ");
-            for (int i=0;i<number;i++){
-                arry[i]=scanner.nextInt();
-            }
-
-//        System.out.print(arr.length);
-            for (int j : arry) {
-                int n = 0;
-
-                if (j < 0) {
-
-                    totalng++;
-                    dne = totalng / arry.length;
-
-                }
-                if (j == 0) {
-
-                    totalze++;
-                    dze = (totalze-1) / arry.length;
-
-                }
-                if (j > 0) {
-
-                    totalpu++;
-                    dpu = totalpu / arry.length;
-                }
-
-
-            }
-            System.out.println("plus "+dpu);
-            System.out.println("negative "+dne);
-            System.out.println("zero "+dze);
-//            System.out.println(totalze);
-//            System.out.println(totalng);
-//            System.out.println(totalpu);
-        }
-
-//        hacker rank task 2
-        public void miniMaxSum(){
-            int number;
-
-            Scanner scanner=new Scanner(System.in);
-            System.out.println("enter the number of elements you want to add");
-            number=scanner.nextInt();
-            int[] c = new int[number];
-            int[] arry1=new int[number];
-            System.out.println("Enter the elements of the array: ");
-            for (int i=0;i<number;i++){
-                arry1[i]=scanner.nextInt();
-            }
-            System.out.println(Arrays.toString(arry1));
-            c=arry1;
-            Arrays.sort(c);
-            System.out.println(Arrays.toString(c));
-            long sum = 0;
-            int min = c[0];
-            int max = c[0];
-            for(int i=0;i<c.length;i++){
-                sum+=c[i];
-                if(c[i]<min){
-                    min = c[i];
-                }
-                if(c[i]>max){
-                    max = c[i];
-                }
-            }
-            System.out.println((sum-max)+" "+(sum-min));
-            System.out.println(sum);
-        }
-//            long min = 0, max = 0;
-//            int count=0;
-//            for(int i = 0, j = c.length - 1; i < c.length - 1; i++, j--){
-//                max = max + c[j];
-//                min = min + c[i];
-//            }
-//            System.out.println(min + " " + max);
-//            System.out.println(count);
-
-
-
 
     }
 
